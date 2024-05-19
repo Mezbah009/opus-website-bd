d488358fcf3143cec6797acfd44ccf342a4624c3
Md.Habibullah Mezbah <mezba@opus-bd.com>
2024-05-19 19:42:24 +0600

    fb link added

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -74,10 +74,9 @@
                 <i class="bi bi-phone-fill phone-icon"></i> +88-01811317129
             </div>
             <div class="social-links d-none d-md-block">
-                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+                <a href="https://www.facebook.com/opustechnologylimited/" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="https://www.linkedin.com/company/opus-technology-limited/" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+                <a href="https://www.youtube.com/@opustechnologylimited" class="youtube"><i class="bi bi-youtube"></i></a>
             </div>
         </div>
     </section>
