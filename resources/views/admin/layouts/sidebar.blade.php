92a44b5b10d8a92eb45ac51435add97b563f3e86
Md.Habibullah Mezbah <mezba@opus-bd.com>
2024-12-26 17:15:16 +0600

    sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('admin.dashboard') }}" class="brand-link">
-        <img src="{{asset('admin-assets/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
+        <img src="{{ asset('admin-assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Opus Bangladesh</span>
     </a>
@@ -9,7 +9,8 @@
     <div class="sidebar">
         <!-- Sidebar user (optional) -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
                 <li class="nav-item">
                     <a href="{{ route('admin.dashboard') }}" class="nav-link">
@@ -41,7 +42,7 @@
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('home_first_sections.index') }}" class="nav-link">
-                                <i class="nav-icon far fa-file-alt"></i>
+                                <i class="nav-icon fas fa-info-circle"></i>
                                 <p>Home First Section</p>
                             </a>
                         </li>
@@ -53,13 +54,14 @@
                         </li> --}}
                         <li class="nav-item">
                             <a href="{{ route('home_services_section.index') }}" class="nav-link">
-                                <i class="nav-icon far fa-file-alt"></i>
+                                <i class="nav-icon fas fa-hands-helping"></i>
                                 <p>Home Service Section</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('testimonials.index') }}" class="nav-link">
-                                <i class="nav-icon fas fa-user"></i>
+                                <i class="nav-icon fas fa-quote-left"></i>
+
                                 <p>Testimonial</p>
                             </a>
                         </li>
@@ -69,7 +71,7 @@
                 <!-- About n=ment with submenus -->
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-home"></i>
+                        <i class="nav-icon fas fa-info-circle"></i>
                         <p>
                             About
                             <i class="right fas fa-angle-left"></i>
@@ -78,31 +80,31 @@
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
                             <a href="{{ route('home_second_sections.index') }}" class="nav-link">
-                                <i class="nav-icon far fa-file-alt"></i>
+                                <i class="nav-icon fas fa-globe"></i>
                                 <p>About First Section</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('accreditation.index') }}" class="nav-link">
-                                <i class="nav-icon far fa-file-alt"></i>
+                                <i class="nav-icon fas fa-ribbon"></i>
                                 <p>Accreditation Section</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('managements.index') }}" class="nav-link">
-                                <i class="nav-icon fas fa-user"></i>
+                                <i class="nav-icon fas fa-users-cog"></i>
                                 <p>Management Section</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('awards.index') }}" class="nav-link">
-                                <i class="nav-icon fas fa-user"></i>
+                                <i class="nav-icon fas fa-trophy"></i>
                                 <p>Awards Section</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('quality.index') }}" class="nav-link">
-                                <i class="nav-icon fas fa-user"></i>
+                                <i class="nav-icon fas fa-shield-alt"></i>
                                 <p>Quality Section</p>
                             </a>
                         </li>
@@ -111,48 +113,48 @@
                 <!-- Other menu items -->
                 <li class="nav-item">
                     <a href="{{ route('products.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-user"></i>
+                        <i class="nav-icon fas fa-box"></i>
                         <p>Product</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('team_members.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-user"></i>
+                        <i class="nav-icon fas fa-user-friends"></i>
                         <p>Team Member</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('clients.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-user"></i>
+                        <i class="nav-icon fas fa-handshake"></i>
                         <p>Clients</p>
                     </a>
                 </li>
 
                 <li class="nav-item">
                     <a href="{{ route('blog.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-user"></i>
+                        <i class="nav-icon fas fa-newspaper"></i>
                         <p>Blogs</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('jobs.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-user"></i>
+                        <i class="nav-icon fas fa-briefcase"></i>
                         <p>Jobs</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('contact.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-user"></i>
+                        <i class="nav-icon fas fa-envelope"></i>
                         <p>Contact Us</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('numbers.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-user"></i>
+                        <i class="nav-icon fas fa-phone"></i>
                         <p>Numbers & Email</p>
                     </a>
                 </li>
-
+                {{--
                 <li class="nav-item">
                     <a href="" class="nav-link">
                         <i class="nav-icon  far fa-file-alt"></i>
@@ -164,7 +166,7 @@
                         <i class="nav-icon fas fa-tags"></i>
                         <p>Product Reviews</p>
                     </a>
-                </li>
+                </li> --}}
                 <li class="nav-item">
                     <a href="" class="nav-link">
                         <i class="nav-icon fas fa-wrench"></i>
