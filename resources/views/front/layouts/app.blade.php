150c80b022d547a35b2ef8d729ee849684f11065
Mezbah009 <hmezbahoffice@gmail.com>
2025-02-04 17:38:25 +0600

    navbar modify

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -97,7 +97,7 @@
     </section>
 
     <!-- ======= Header ======= -->
-    <header id="header" class="d-flex align-items-center">
+    {{-- <header id="header" class="d-flex align-items-center">
         <div class="container d-flex align-items-center">
             <div class="logo me-auto">
                 <!-- <h1><a href="index.html">OPUS</a></h1> -->
@@ -136,7 +136,49 @@
                 <i class="bi bi-list mobile-nav-toggle"></i>
             </nav><!-- .navbar -->
         </div>
+    </header><!-- End Header --> --}}
+
+
+    <header id="header" class="d-flex align-items-center">
+        <div class="container d-flex align-items-center">
+            <div class="logo me-auto">
+                <!-- <h1><a href="index.html">OPUS</a></h1> -->
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <a href="{{ route('front.home') }}"><img src="{{ asset('front-assets/img/opus-logo.png') }}" alt="" class="img-fluid"></a>
+            </div>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto {{ Request::is('/') ? 'active' : '' }}" href="{{ route('front.home') }}">Home</a></li>
+                    <li><a class="nav-link scrollto {{ Request::is('about-us') ? 'active' : '' }}" href="{{ route('front.about') }}">About</a></li>
+
+                    <!-- Dropdown Menu for Product -->
+                    <li class="dropdown">
+                        <a href="#" class="nav-link scrollto dropdown-toggle {{ Request::is('products') ? 'active' : '' }}" data-bs-toggle="dropdown">Product</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="{{ route('front.products') }}">Enterprise</a></li>
+                            <li><a class="dropdown-item" href="{{ route('front.fintech') }}">Fintech</a></li>
+                            <li><a class="dropdown-item" href="">Mobile App</a></li>
+                            <li><a class="dropdown-item" href="">AI</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a class="nav-link scrollto {{ Request::is('clients') ? 'active' : '' }}" href="{{ route('front.clients') }}">Clients</a></li>
+                    <li><a class="nav-link scrollto {{ Request::is('services') ? 'active' : '' }}" href="{{ route('front.services') }}">Services</a></li>
+                    <li><a class="nav-link scrollto {{ Request::is('blogs') ? 'active' : '' }}" href="{{ route('front.blog') }}">Blogs</a></li>
+                    <li><a class="nav-link scrollto {{ Request::is('jobs') ? 'active' : '' }}" href="{{ route('front.jobs') }}">Jobs</a></li>
+                    <li><a class="nav-link scrollto {{ Request::is('contact-us') ? 'active' : '' }}" href="{{ route('front.contact') }}">Contact</a></li>
+
+                    <!-- Request Demo Button -->
+                    <li>
+                        <a class="btn-primary text-whites" href="{{ route('front.demo') }}">Request Demo</a>
+                    </li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+        </div>
     </header><!-- End Header -->
+
 
 
 
