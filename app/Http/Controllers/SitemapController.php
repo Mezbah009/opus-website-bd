7bfb2ac1cd8fa898ec312ecfe4281aac3b662bb8
Mezbah009 <hmezbahoffice@gmail.com>
2025-03-04 11:12:37 +0600

    meta tag added

---
+++ app/Http/Controllers/SitemapController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\URL;
+
+class SitemapController extends Controller
+{
+    public function generate()
+    {
+        $urls = [
+            URL::to('/'),
+            URL::to('/about-us'),
+            URL::to('/products'),
+            URL::to('/services'),
+            URL::to('/clients'),
+            URL::to('/case-study'),
+            URL::to('/blogs'),
+            URL::to('/jobs'),
+            URL::to('/contact-us'),
+        ];
+
+        $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
+        $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+        foreach ($urls as $url) {
+            $sitemap .= '<url>';
+            $sitemap .= '<loc>' . $url . '</loc>';
+            $sitemap .= '<lastmod>' . now()->toAtomString() . '</lastmod>';
+            $sitemap .= '<changefreq>weekly</changefreq>';
+            $sitemap .= '<priority>0.8</priority>';
+            $sitemap .= '</url>';
+        }
+
+        $sitemap .= '</urlset>';
+
+        return Response::make($sitemap, 200, ['Content-Type' => 'application/xml']);
+    }
+
+
+    // robots.txt
+
+
+    public function robots()
+    {
+        $content = "User-agent: *\n";
+        $content .= "Disallow: /admin/\n";
+        $content .= "Disallow: /login/\n";
+        $content .= "Disallow: /register/\n";
+        $content .= "Allow: /\n";
+        $content .= "Sitemap: " . url('/sitemap.xml') . "\n";
+
+        return response($content, 200)
+            ->header('Content-Type', 'text/plain');
+    }
+}
