0ea2e4bcf0ad6fb360df5453f776e7373abdfbca
Mezbah009 <hmezbahoffice@gmail.com>
2025-07-17 15:51:19 +0600

    tag

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <!-- Canonical URL -->
     {{-- <link rel="canonical" href="{{ url()->current() }}"> --}}
     <link rel="canonical" href="{{ 'https://opus-bd.com' . request()->getRequestUri() }}">
-    
+
 
 
 
@@ -63,6 +63,10 @@
         ]
         }
         </script>
+
+
+
+
 
     <!-- Google Fonts -->
     <link
@@ -159,12 +163,38 @@
     <!-- Google search console -->
     <meta name="google-site-verification" content="vN7lmALCAdHCPjvy7MsmlHlxlrWetMdHv2gz8kKuNAM" />
 
+
+
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-KBSRQX24');
+    </script>
+    <!-- End Google Tag Manager -->
+
 </head>
 
 <body>
 
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CD4JCQS" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KBSRQX24" height="0" width="0"
             style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
